dfdfe2f46b34cd4e724a5a0df0d90434912d7919
Naresh <koppera.kumar@tigerspike.com>
2022-05-20 10:15:24 +0530

    Updated UI for tap screen

--- PayRowSDK/src/main/java/com/payrow/tappay/PayRowActivity.kt
+++ PayRowSDK/src/main/java/com/payrow/tappay/PayRowActivity.kt
@@ -20,6 +20,7 @@ class PayRowActivity : AppCompatActivity(), EMVAdapterListener {
     private var txRef = 0
     private var amount = 0
     private val TAG=PayRowActivity::class.simpleName
+    private var countdownInterfaceInitialized = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -92,6 +93,17 @@ class PayRowActivity : AppCompatActivity(), EMVAdapterListener {
     override fun onCardProcessing() {
         Log.d(TAG,"CardProcessing")
        // showToast("CardProcessing")
+        runOnUiThread {
+            try {
+                println("SDK PaymentActivity On Card processing")
+                (findViewById(R.id.textViewTapToPay) as TextView).text = "Please Wait..."
+                (findViewById(R.id.textViewSecondsLeft) as TextView).text = ""
+                (findViewById(R.id.textViewTimeoutSeconds) as TextView).text = ""
+                (findViewById(R.id.buttonCancel) as Button).visibility = View.INVISIBLE
+                countdownInterfaceInitialized = false
+            } catch (ex: Exception) {
+            }
+        }
     }
 
     override fun onCardProcessingComplete() {
@@ -100,10 +112,22 @@ class PayRowActivity : AppCompatActivity(), EMVAdapterListener {
 
     override fun onCardProcessingNotify(message: String) {
         Log.d(TAG,"CardProcessingNotify  $message")
+        runOnUiThread {
+            (findViewById(R.id.textViewCardProcessingNotify) as TextView).text = message
+        }
     }
 
     override fun onCardRemoved() {
         Log.d(TAG,"Card Removed")
+        runOnUiThread {
+            try {
+                (findViewById(R.id.textViewTapToPay) as TextView).text = "Please wait..."
+                (findViewById(R.id.textViewSecondsLeft) as TextView).text = ""
+                (findViewById(R.id.textViewTimeoutSeconds) as TextView).text = ""
+                setContentView(R.layout.activity_please_wait)
+            } catch (ex: Exception) {
+            }
+        }
     }
 
     override fun onCheckDeviceRegistrationComplete(
@@ -128,17 +152,31 @@ class PayRowActivity : AppCompatActivity(), EMVAdapterListener {
         sessionData: Map<String, String>
     ) {
         Log.d(TAG,"SessionInitComplete  $isSuccessful  // $reason")
+        showToast(reason)
+        finish()
     }
 
     override fun onSessionCountdown(remainingSeconds: Int) {
         Log.d(TAG,"Session count  $remainingSeconds")
+        try {
+            runOnUiThread {
+                if (!countdownInterfaceInitialized) {
+                    (findViewById(R.id.textViewTapToPay) as TextView).text = "Tap Card To Pay"
+                    (findViewById(R.id.textViewSecondsLeft) as TextView).text = "Seconds Left"
+                    (findViewById(R.id.buttonCancel) as Button).visibility = View.VISIBLE
+                    countdownInterfaceInitialized = true
+                }
+                (findViewById(R.id.textViewTimeoutSeconds) as TextView).text =
+                    remainingSeconds.toString()
+            }
+        } catch (ex: Exception) {
+        }
     }
 
     override fun onSessionInitComplete(isInitialized: Boolean, reason: String) {
         Log.d(TAG,"SessionInitComplete  $isInitialized  // $reason")
         try {
             txRef += 1
-            //Not implemented
             if (isInitialized) {
                 runOnUiThread {
                     setContentView(R.layout.tap_to_pay)
