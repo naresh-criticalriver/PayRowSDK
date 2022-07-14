568a171391107fb1c44c131ba8397a06537279cd
Sudheerakula <sudheer.akula@tigerspike.com>
2022-07-14 12:49:36 +0530

    #mplemented card reader sdk using NFC

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -1,104 +1,104 @@
-package com.example.sampleapp
-
-import android.app.Activity
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import com.a.b.EMVAdapterListener
-import com.payrow.tappay.PayRowActivity
-import com.payrow.tappay.PayRowAdapterListener
-import com.payrow.tappay.PayRowConfig
-import com.payrow.tappay.PayRowEMVAdapter
-import java.text.DecimalFormat
-
-
-class MainActivity : AppCompatActivity() {
-
-    val TAG = "TapPay"
-    private val merchantToken = "1234567788="
-    private var txtRef = 0
-    private var amount = 100
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-      //  DashboardActivity.displayToast("Welcome", this)
-
-       // val txtRefId = DecimalFormat("00000").format(txRef)
-
-        val payRowConfig = PayRowConfig(
-            "0784",
-            "0784",
-            1,
-            100.00,
-            0,
-            "00",
-            2,
-            "00",
-            false,
-            "01",
-        )
-
-     /*   //Initializing PayRowAdapter
-       val payRowEMVAdapter= PayRowEMVAdapter()
-        payRowEMVAdapter.initPayRowAdapter(this, payRowConfig, this)
-
-      //  payRowEMVAdapter.initPayRowSession(  merchantToken, txtRefId, amount)
-
-//        For cancelling current session
-       // payRowEMVAdapter.cancelCurrentSession()*/
-
-      /*  startActivity(
-            )*/
-       /* val intent = Intent(this, PayRowActivity::class.java)
-            .putExtra(PayRowActivity.TXT_REF_ID, txtRef)
-            .putExtra(PayRowActivity.AMOUNT, amount)
-      //  startActivityForResult(intent,PayRowActivity.PAYROW_RESULT_CODE)
-        resultLauncher.launch(intent)*/
-
-
-        findViewById<AppCompatButton>(R.id.paynow).setOnClickListener {
-            val intent = Intent(this, PayRowActivity::class.java)
-                .putExtra(PayRowActivity.TXT_REF_ID, txtRef)
-                .putExtra(PayRowActivity.AMOUNT, amount)
-            //  startActivityForResult(intent,PayRowActivity.PAYROW_RESULT_CODE)
-            resultLauncher.launch(intent)
-        }
-
-    }
-
-    fun showToast(msg:String){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
-    }
-
-    var resultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                val data: Intent? = result.data
-                // doSomeOperations()
-                result.data?.let {
-                    it.getStringExtra(PayRowActivity.RESULT_MSG)?.let {reason->
-                        Log.d(TAG,reason)
-                        showToast(reason)
-                    }
-                }
-            }else if(result.resultCode == Activity.RESULT_CANCELED){
-                val data: Intent? = result.data
-                // doSomeOperations()
-                result.data?.let {
-                    it.getStringExtra(PayRowActivity.RESULT_MSG)?.let {reason->
-                        Log.d(TAG,reason)
-                        showToast(reason)
-                    }
-                }
-            }
-        }
-    }
+//package com.example.sampleapp
+//
+//import android.app.Activity
+//import android.content.Intent
+//import android.os.Bundle
+//import android.util.Log
+//import android.widget.Button
+//import android.widget.Toast
+//import androidx.activity.result.contract.ActivityResultContracts
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.appcompat.widget.AppCompatButton
+//import com.a.b.EMVAdapterListener
+//import com.payrow.tappay.PayRowActivity
+//import com.payrow.tappay.PayRowAdapterListener
+//import com.payrow.tappay.PayRowConfig
+//import com.payrow.tappay.PayRowEMVAdapter
+//import java.text.DecimalFormat
+//
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    val TAG = "TapPay"
+//    private val merchantToken = "1234567788="
+//    private var txtRef = 0
+//    private var amount = 100
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//      //  DashboardActivity.displayToast("Welcome", this)
+//
+//       // val txtRefId = DecimalFormat("00000").format(txRef)
+//
+//        val payRowConfig = PayRowConfig(
+//            "0784",
+//            "0784",
+//            1,
+//            100.00,
+//            0,
+//            "00",
+//            2,
+//            "00",
+//            false,
+//            "01",
+//        )
+//
+//     /*   //Initializing PayRowAdapter
+//       val payRowEMVAdapter= PayRowEMVAdapter()
+//        payRowEMVAdapter.initPayRowAdapter(this, payRowConfig, this)
+//
+//      //  payRowEMVAdapter.initPayRowSession(  merchantToken, txtRefId, amount)
+//
+////        For cancelling current session
+//       // payRowEMVAdapter.cancelCurrentSession()*/
+//
+//      /*  startActivity(
+//            )*/
+//       /* val intent = Intent(this, PayRowActivity::class.java)
+//            .putExtra(PayRowActivity.TXT_REF_ID, txtRef)
+//            .putExtra(PayRowActivity.AMOUNT, amount)
+//      //  startActivityForResult(intent,PayRowActivity.PAYROW_RESULT_CODE)
+//        resultLauncher.launch(intent)*/
+//
+//
+//        findViewById<AppCompatButton>(R.id.paynow).setOnClickListener {
+//            val intent = Intent(this, PayRowActivity::class.java)
+//                .putExtra(PayRowActivity.TXT_REF_ID, txtRef)
+//                .putExtra(PayRowActivity.AMOUNT, amount)
+//            //  startActivityForResult(intent,PayRowActivity.PAYROW_RESULT_CODE)
+//            resultLauncher.launch(intent)
+//        }
+//
+//    }
+//
+//    fun showToast(msg:String){
+//        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+//    }
+//
+//    var resultLauncher =
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if (result.resultCode == Activity.RESULT_OK) {
+//                // There are no request codes
+//                val data: Intent? = result.data
+//                // doSomeOperations()
+//                result.data?.let {
+//                    it.getStringExtra(PayRowActivity.RESULT_MSG)?.let {reason->
+//                        Log.d(TAG,reason)
+//                        showToast(reason)
+//                    }
+//                }
+//            }else if(result.resultCode == Activity.RESULT_CANCELED){
+//                val data: Intent? = result.data
+//                // doSomeOperations()
+//                result.data?.let {
+//                    it.getStringExtra(PayRowActivity.RESULT_MSG)?.let {reason->
+//                        Log.d(TAG,reason)
+//                        showToast(reason)
+//                    }
+//                }
+//            }
+//        }
+//    }
